09d3508aa7ae7cdf43f57b9607369aaaf729080a
Aaron Maenpaa <aaron@maenpaa.ca>
2013-04-05 11:32:33 -0400

    It's called AlchemySharp.
    ... it's only inspired by SQLAlchemy.

--- AlchemySharp/AlchemySharp/Properties/AssemblyInfo.cs
+++ AlchemySharp/AlchemySharp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLAlchemy")]
+[assembly: AssemblyTitle("AlchemySharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SQLAlchemy")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyProduct("AlchemySharp")]
+[assembly: AssemblyCopyright("Copyright © Fog Creek Software 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
